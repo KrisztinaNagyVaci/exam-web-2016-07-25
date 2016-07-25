09bc6de50741cef127c0019c6cb79157401db306
Kriszti <krisztina.vaci@gmail.com>
2016-07-25 20:07:59 +0200

    Frontend rewritten in rev modul pattern

--- client/frontend.js
+++ client/frontend.js
@@ -1,41 +1,70 @@
 'use strict';
 
-var url = 'http://localhost:3000/decode';
+var controller = (function(){
 
-var numberInput = document.querySelector('.number_input');
-var textInput = document.querySelector('.text_input');
-var sendButton = document.querySelector('.send_button');
-sendButton.addEventListener('click', addElement);
-
-function addElement(){
-  if (numberInput.value === '' || textInput.value === '' || (!/^[a-z A-Z]+$/.test(textInput.value))) {
-    alert('Please fill in both input fields and only use letters!')
-  };
-  var inputData = JSON.stringify({
-    'shift' : numberInput.value,
-    'text' : textInput.value
-  });
-  callAPI(createElement, 'POST', url, true, inputData);
-};
-
-function callAPI(callback, method, url, value, senddata){
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status >= 200){
-          var response = JSON.parse(xhr.response);
-          callback(response);
-        }
-    }
-    xhr.open(method, url, value);
-    xhr.setRequestHeader("content-type", "application/json; charset=utf-8");
-    console.log('Loading');
-    xhr.send(senddata);
+  function addElement(){
+    if (DOMfunctions.numberInput.value === '' || DOMfunctions.textInput.value === '' || (!/^[a-z A-Z]+$/.test(DOMfunctions.textInput.value))) {
+      alert('Please fill in both input fields and only use letters!')
+    };
+    var inputData = JSON.stringify({
+      'shift' : DOMfunctions.numberInput.value,
+      'text' : DOMfunctions.textInput.value
+    });
+    APIfunctions.callAPI(DOMfunctions.createElement, 'POST', APIfunctions.url, true, inputData);
   };
 
+  return {
+    addElement: addElement
+  }
 
-function createElement(response){
-  var decryptedItems = document.querySelector('.decrypted_items');
-  var pTag = document.createElement('p');
-  decryptedItems.appendChild(pTag);
-  pTag.innerHTML = response.text;
-}
+})();
+
+
+var APIfunctions = (function(){
+
+  var url = 'http://localhost:3000/decode';
+
+  function callAPI(callback, method, url, value, senddata){
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status >= 200){
+            var response = JSON.parse(xhr.response);
+            callback(response);
+          }
+      }
+      xhr.open(method, url, value);
+      xhr.setRequestHeader("content-type", "application/json; charset=utf-8");
+      console.log('Loading');
+      xhr.send(senddata);
+    };
+
+  return {
+    callAPI: callAPI,
+    url: url
+  }
+
+})();
+
+
+var DOMfunctions = (function(){
+
+  var numberInput = document.querySelector('.number_input');
+  var textInput = document.querySelector('.text_input');
+  var sendButton = document.querySelector('.send_button');
+  sendButton.addEventListener('click', controller.addElement);
+
+  function createElement(response){
+    var decryptedItems = document.querySelector('.decrypted_items');
+    var pTag = document.createElement('p');
+    decryptedItems.appendChild(pTag);
+    pTag.innerHTML = response.text;
+  }
+
+  return {
+    createElement: createElement,
+    numberInput: numberInput,
+    textInput: textInput,
+    sendButton: sendButton
+  }
+
+})();
